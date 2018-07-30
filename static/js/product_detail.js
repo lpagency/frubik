95677dd6e35f4daa07c3f2076a51514db9458670
Ricardo Silva <ricardo@loadingplay.com>
2018-07-30 18:32:42 -0400

    removed site_id

--- static/js/product_detail.js
+++ static/js/product_detail.js
@@ -1,7 +1,7 @@
 /* global $ */
 'use strict';
 
-$(document).ready(function() 
+$(document).ready(function()
 {
     var base_url = $.environmentVar(
         'https://apibodegas.loadingplay.com/',
@@ -11,11 +11,11 @@ $(document).ready(function()
         'https://pay.loadingplay.com',
         'https://pay.loadingplay.com',
         'https://pay.loadingplay.com');
-    var app_public = $.environmentVar(20,20,20);
+    var app_public = $.environmentVar('blend','blend','blend');
 
-    // functions 
+    // functions
     // productos relacionados
-    var related = function(tag) 
+    var related = function(tag)
     {
         var config = {
             'app_public': app_public,
@@ -29,7 +29,7 @@ $(document).ready(function()
         $('.product-related').ecommerce('product_box', config);
     };
 
-    //cambia imagenes pequeñas en detalle de producto 
+    //cambia imagenes pequeñas en detalle de producto
     $(document).on("click", '.little', function(){
         $("#img_detail").attr("src", $(this).attr('src'));
     });
